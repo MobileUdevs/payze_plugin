97960a8d69534f419995fc6a9261b3bb1cc15350
Sunnatillo Shavkatov <sunnatillo.shavkatov@udevs.io>
2022-12-03 18:51:34 +0500

    paye integration done

--- ios/Classes/SwiftPayzePlugin.swift
+++ ios/Classes/SwiftPayzePlugin.swift
@@ -1,14 +1,37 @@
 import Flutter
+import Payze_iOS_sdk
 import UIKit
 
 public class SwiftPayzePlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "payze_plugin", binaryMessenger: registrar.messenger())
-    let instance = SwiftPayzePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "payze_plugin", binaryMessenger: registrar.messenger())
+        let instance = SwiftPayzePlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if (call.method == "payze_open") {
+            let args = call.arguments as? [String:Any]
+            let number = args?["number"] as! String
+            let cardHolder = args?["cardHolder"] as! String
+            let expirationDate = args?["expirationDate"] as! String
+            let securityNumber = args?["securityNumber"] as! String
+            let transactionId = args?["transactionId"] as! String
+            let billingAdress = args?["billingAddress"] as! String
+            guard let paymentDetails = PaymentDetails.init(number: number,
+                                                           cardHolder: cardHolder,
+                                                           expirationDate: expirationDate,
+                                                           securityNumber: securityNumber,
+                                                           transactionId: transactionId,
+                                                           billingAddress: billingAdress) else {
+                result(false)
+                return
+            }
+            PaymentService.shared.startPayment(paymentDetails: paymentDetails) { results in
+                print(results)
+                result(true)
+            }
+        }
+        
+    }
 }
--- ios/payze_plugin.podspec
+++ ios/payze_plugin.podspec
@@ -14,9 +14,9 @@ A new Flutter project.
   s.author           = { 'Your Company' => 'email@example.com' }
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
+  s.dependency 'Payze-iOS-sdk', "~> 0.1.0"
   s.dependency 'Flutter'
-  s.platform = :ios, '11.0'
-
+  s.platform = :ios, '12.0'
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
   s.swift_version = '5.0'
